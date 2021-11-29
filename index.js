60eb7a8338dbebdc4e9578b5d909fcbaa8617521
SHIFTTECH\NiyaazM <niyaaz.majiet@rank.com>
2021-11-29 22:43:29 +0200

    updated api's

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Run = require('run-sdk');
+const Pusher = require("pusher");
+
 
 const run = new Run({
     network: 'mock',
@@ -7,14 +9,12 @@ const run = new Run({
 
 run.trust('*');
 
-const Pusher = require("pusher");
-
 const pusher = new Pusher({
-  appId: "1307097",
-  key: "9f3bccfa32eb736db419",
-  secret: "a3ad9a8970b498ee5952",
-  cluster: "eu",
-  useTLS: true
+    appId: "1307097",
+    key: "9f3bccfa32eb736db419",
+    secret: "a3ad9a8970b498ee5952",
+    cluster: "eu",
+    useTLS: true
 });
 
 const bodyParser = require('body-parser');
@@ -26,142 +26,160 @@ app.use(bodyParser.json());
 const port = 3000;
 
 app.get('/getJigState', async (req, res) => {
-    const { origin } = req.query;
-    const getJig = async () =>{
-      try {
-        const transferJig = await run.load(origin);
-        await transferJig.sync();
-        console.log(transferJig.creatorAddress);
-        return {
-            creatorAddress:transferJig.creatorAddress,
-            creatorAmount:transferJig.creatorAmount,
-            creatorBlockchain:transferJig.creatorBlockchain,
-            creatorEscrowAddress:transferJig.creatorEscrowAddress,
-            creatorTxId:transferJig.creatorTxId,
-            currentJigState:transferJig.currentJigState,
-            currentState:transferJig.currentState,
-            location:transferJig.location,
-            nonce:transferJig.nonce,
-            origin:transferJig.origin,
-            owner:transferJig.owner,
-            receiverAddress:transferJig.receiverAddress,
-            receiverAmount:transferJig.receiverAmount,
-            receiverBlockchain:transferJig.receiverBlockchain,
-            receiverEscrowAddress:transferJig.receiverEscrowAddress,
-            receiverTxId:transferJig.receiverTxId,
-            satoshis:transferJig.satoshis 
-        };
-      } catch (error) {
-        console.log(error)
-      return null
-      }
+    const {origin} = req.query;
+    const getJig = async () => {
+        try {
+            const transferJig = await run.load(origin);
+            await transferJig.sync();
+            return {
+                creatorAddress: transferJig.creatorAddress,
+                creatorAmount: transferJig.creatorAmount,
+                creatorBlockchain: transferJig.creatorBlockchain,
+                creatorEscrowAddress: transferJig.creatorEscrowAddress,
+                creatorTxId: transferJig.creatorTxId,
+                currentJigState: transferJig.currentJigState,
+                currentState: transferJig.currentState,
+                location: transferJig.location,
+                nonce: transferJig.nonce,
+                origin: transferJig.origin,
+                owner: transferJig.owner,
+                receiverAddress: transferJig.receiverAddress,
+                receiverAmount: transferJig.receiverAmount,
+                receiverBlockchain: transferJig.receiverBlockchain,
+                receiverEscrowAddress: transferJig.receiverEscrowAddress,
+                receiverTxId: transferJig.receiverTxId,
+                satoshis: transferJig.satoshis
+            };
+        } catch (error) {
+            console.log(error)
+            return null
+        }
     };
-    
+
     const jigData = await getJig();
     res.json({jigData});
-  });
+});
 
 app.post('/createNewJig', async (req, res) => {
-  console.log('creating jigr')
-  const {owner,creatorEscrowAddress,receiverEscrowAddress,creatorBlockchain,receiverBlockchain,creatorAmount,receiverAmount,receiverAddress,creatorAddress} = req.body;
-  const createTransferStateJig = async () =>{
-    try {
-    const jig = new TransferState(owner,creatorEscrowAddress,receiverEscrowAddress,creatorBlockchain,receiverBlockchain,creatorAmount,receiverAmount,receiverAddress,creatorAddress);
+    const {
+        owner,
+        creatorEscrowAddress,
+        receiverEscrowAddress,
+        creatorBlockchain,
+        receiverBlockchain,
+        creatorAmount,
+        receiverAmount,
+        receiverAddress,
+        creatorAddress
+    } = req.body;
+    const createTransferStateJig = async () => {
+        try {
+            const jig = new TransferState(owner, creatorEscrowAddress, receiverEscrowAddress, creatorBlockchain, receiverBlockchain, creatorAmount, receiverAmount, receiverAddress, creatorAddress);
 
-    await jig.sync();
+            await jig.sync();
 
-    console.log("origin",jig.origin);
-    console.log(jig)
-    return jig.origin;
-    } catch (error) {
-      console.log(error)
-    return null;
-    }
-  };
+            console.log("origin", jig.origin);
+            console.log(jig)
+            return jig.origin;
+        } catch (error) {
+            console.log(error)
+            return null;
+        }
+    };
 
-  const origin = await createTransferStateJig();
+    const origin = await createTransferStateJig();
 
-  res.json({origin});
+    res.json({origin});
 });
 
 app.post('/updateReceiverTxId', async (req, res) => {
-    const { receiverTxId, origin } = req.body;
-  
-    const updateReceiverTxId = async () =>{
-      try {
-        const transferJig = await run.load(origin);
-        await transferJig.sync();
-        const tx = new Run.Transaction()
+    const {receiverTxId, origin} = req.body;
 
-        await tx.update(() => {
-          transferJig.updateReceiverTxId(receiverTxId);
-        })
-        
-        console.log(transferJig.receiverTxId);
+    const updateReceiverTxId = async () => {
+        try {
+            const transferJig = await run.load(origin);
+            await transferJig.sync();
+            const tx = new Run.Transaction();
 
-        return {updated : true}
-      } catch (error) {
-        console.log(error);
-        return {updated : false}
-      }
+            await tx.update(() => {
+                transferJig.updateReceiverTxId(receiverTxId);
+            })
+
+            console.log(transferJig.receiverTxId);
+            console.log(transferJig);
+
+            return {updated: true}
+        } catch (error) {
+            console.log(error);
+            return {updated: false}
+        }
     }
-  
+
     const updated = await updateReceiverTxId();
-    res.json({updated});
+    res.json(updated);
 });
 
 app.post('/updateCreatorTxId', (req, res) => {
-    const { creatorTxId, origin } = req.body;
-  
-    const updateCreatorTxId = async () =>{
-      try {
-        const transferJig = await run.load(origin);
-        await transferJig.sync();
-        transferJig.updateCreatorTxId(creatorTxId);
-        await transferJig.sync();
+    const {creatorTxId, origin} = req.body;
 
-        console.log(transferJig.creatorTxId)
+    const updateCreatorTxId = async () => {
+        try {
+            const transferJig = await run.load(origin);
 
-        return {updated : true}
-      } catch (error) {
-        console.log(error);
-        return {updated : false}
-      }
+            await transferJig.sync();
+
+            const tx = new Run.Transaction();
+
+            await tx.update(() => {
+                transferJig.updateCreatorTxId(creatorTxId);
+            });
+
+            console.log(transferJig.creatorTxId)
+
+            return {updated: true}
+        } catch (error) {
+            console.log(error);
+            return {updated: false}
+        }
     }
-  
+
     const updated = updateCreatorTxId();
-    res.json({updated});
+    res.json(updated);
 });
 
 app.post('/completeTransfer', (req, res) => {
-    const { origin } = req.body;
-  
-    const completeTransfer = async () =>{
-      try {
-        const transferJig = await run.load(origin);
-        await transferJig.sync();
-        transferJig.completeTransfer();
-        await transferJig.sync();
+    const {origin} = req.body;
 
-        console.log(transferJig.currentState);
+    const completeTransfer = async () => {
+        try {
+            const transferJig = await run.load(origin);
 
-        return {updated : true}
-      } catch (error) {
-        console.log(error);
-        return {updated : false}
-      }
+            await transferJig.sync();
+
+            const tx = new Run.Transaction();
+
+            await tx.update(() => {
+                transferJig.completeTransfer();
+            });
+
+            console.log(transferJig.currentState);
+
+            return {updated: true}
+        } catch (error) {
+            console.log(error);
+            return {updated: false}
+        }
     }
-  
+
     const updated = completeTransfer();
-    res.json({updated});
+    res.json(updated);
 });
 
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
 
-
 class TransferState extends Jig {
-    init(owner, creatorEscrowAddress,receiverEscrowAddress,creatorBlockchain,receiverBlockchain,creatorAmount,receiverAmount,receiverAddress,creatorAddress){
+    init(owner, creatorEscrowAddress, receiverEscrowAddress, creatorBlockchain, receiverBlockchain, creatorAmount, receiverAmount, receiverAddress, creatorAddress) {
         this.owner = owner;
         this.creatorBlockchain = creatorBlockchain;
         this.creatorEscrowAddress = creatorEscrowAddress;
@@ -177,32 +195,31 @@ class TransferState extends Jig {
         this._updateJigState();
     }
 
-    _updateJigState(){
-        const states = ['Initialized','Waiting Transactions','Recieved Transactions','Completed'];
+    _updateJigState() {
+        const states = ['Initialized', 'Waiting Transactions', 'Recieved Transactions', 'Completed'];
         const indexOfCurrentState = states.indexOf(this.currentState);
-        if(indexOfCurrentState >= states.length - 1){
-           throw Error('Already Completed');
-        }else{
-         this.currentState = states[indexOfCurrentState + 1];
+        if (indexOfCurrentState >= states.length - 1) {
+            throw Error('Already Completed');
+        } else {
+            this.currentState = states[indexOfCurrentState + 1];
         }
     }
 
-    updateReceiverTxId(receiverTxId){
-       this.receiverTxId = receiverTxId;
-       if(this.creatorTxId){
-         this._updateJigState()
-       }
-    }
-
-    updateCreatorTxId(creatorTxId){
-        this.creatorTxId = creatorTxId;
-        if(this.receiverTxId){
+    updateReceiverTxId(receiverTxId) {
+        this.receiverTxId = receiverTxId;
+        if (this.creatorTxId) {
             this._updateJigState()
         }
     }
 
-    completeTransfer(){
-      this._updateJigState();
+    updateCreatorTxId(creatorTxId) {
+        this.creatorTxId = creatorTxId;
+        if (this.receiverTxId) {
+            this._updateJigState()
+        }
+    }
+
+    completeTransfer() {
+        this._updateJigState();
     }
 }
-
